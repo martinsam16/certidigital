9de00cc64d0ea3fae6aef378eb8298345d1d37d2
Martín Alexis Samán Arata <martin.saman@vallegrande.edu.pe>
2020-12-27 19:33:19 -0500

    feat: add configuration for lacchain

--- contracts/Certidigital.sol
+++ contracts/Certidigital.sol
@@ -7,21 +7,31 @@ contract Certidigital {
 
     struct Certificado {
         address emisor;
+        address receptor;
         string tituloCertificado;
+        string fecha;
+        string fechaExpiracion;
     }
 
-    mapping(address => Certificado) certificados;
+    //id, certificado
+    mapping(string => Certificado) certificados;
 
     event Certificar(address indexed _from, address indexed _to, Certificado _certificado);
 
-    function crearCertificado(address receptor, string memory tituloCertificado) public {
-        Certificado memory certificado = Certificado(msg.sender, tituloCertificado);
-        certificados[receptor] = certificado;
+    function crearCertificado(address receptor, string memory idCertificado, string memory tituloCertificado, string memory fecha, string memory fechaExpiracion) public
+    returns (Certificado memory) {
+        Certificado memory certificado = Certificado(msg.sender, receptor, tituloCertificado, fecha, fechaExpiracion);
+        certificados[idCertificado] = certificado;
         emit Certificar(msg.sender, receptor, certificado);
+        return certificado;
     }
 
-    function getCertificado(address direccion) public view returns (Certificado memory){
-        return certificados[direccion];
+    function eliminarCertificado(string memory idCertificado) public {
+        delete certificados[idCertificado];
+    }
+
+    function getCertificado(string memory idCertificado) public view returns (Certificado memory){
+        return certificados[idCertificado];
     }
 
 }
